a56e1b0dbe895ff17b44ecee705dd72b6ad21d98
Dennis Demir <dennisjdemir@gmail.com>
2023-08-19 14:02:26 -0400

    build user table data model

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column, DateTime, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship, backref
+from datetime import datetime
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,3 +10,17 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id=Column(Integer(), primary_key=True)
+    name=Column(String())
+    age=Column(Integer())
+
+    tasks = relationship('Task', backref=backref('users'))
+
+    def __repr__(self):
+        return f'User (ID: {self.id}, ' + \
+            f'Name: {self.name}, ' + \
+            f'Age: {self.age})'
