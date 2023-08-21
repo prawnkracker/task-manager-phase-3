f08d977bcc10251ac676cb9d665447711d047029
Dennis Demir <dennisjdemir@gmail.com>
2023-08-21 08:35:01 -0400

    add functionality to clear records before seeding data

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+import random
 from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,14 +11,22 @@ engine = create_engine('sqlite:///tasks_manager.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+def delete_records():
+    session.query(User).delete()
+    session.query(Task).delete()
+    session.commit()
+
 def create_records():
     users = [User(
         name=f'{fake.name()}',
-        age=randint(16, 65)
+        age=random.randint(16, 65)
     ) for i in range(10)]
 
     tasks = [Task(
-        todo=f'{fake.sentence(nb_words=6)}'
-        completed= choice([True, False])
-        date_added=
-    )]
+        todo=f'{fake.sentence(nb_words=6)}',
+        completed= random.choice([True, False])
+    ) for i in range(30)]
+    
+    session.add_all(users + tasks)
+    session.commit()
+    return users, tasks
