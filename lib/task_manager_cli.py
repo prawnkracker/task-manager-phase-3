440adf5c3041e54417a7e8569f750a12399136bc
Dennis Demir <dennisjdemir@gmail.com>
2023-08-23 15:08:07 -0400

    add __init__ method to store required info upon startup

--- lib/task_manager_cli.py
+++ lib/task_manager_cli.py
@@ -3,6 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from db.models import User, Task
 
+class TaskManager:
+    def __init__(self):
+        self.users=[user for user in session.query(User)]
+        self.tasks=[task for task in session.query(Task)]
+        self.session=session
+
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///db/tasks_manager.db")
