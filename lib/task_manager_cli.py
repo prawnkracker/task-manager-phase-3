db3a144a7e69570dee2c23bf7b1893f8ef4accf1
Dennis Demir <dennisjdemir@gmail.com>
2023-08-23 15:23:26 -0400

    add method for starting point of cli

--- lib/task_manager_cli.py
+++ lib/task_manager_cli.py
@@ -9,6 +9,17 @@ class TaskManager:
         self.tasks=[task for task in session.query(Task)]
         self.session=session
 
+    def starting_page(self):
+        print("Welcome to Task Manager")
+        print("")
+        print("Option Select:")
+        print("")
+        print("Press C to create new entries!")
+        print("Press R to read from the database!")
+        print("Press U to update entries!")
+        print("Press D to delete entries!")
+        print("")
+        print("Press X to exit the CLI!")
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///db/tasks_manager.db")
